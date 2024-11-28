b810aca37bdb05075ad36e55efc78575464f3622
Gungun974 <xfelix974@gmail.com>
2024-11-28 14:39:51 +0400

    Keep cloning even when git say 500

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -7,12 +7,33 @@
 mkdir -p deps && cd deps
 
 # mbedtls
-[ ! -d mbedtls ] && git clone --depth 1 --branch v$v_mbedtls --recurse-submodules https://github.com/Mbed-TLS/mbedtls.git mbedtls
+while true; do
+    rm -rf mbedtls
+    if git clone --depth 1 --branch v$v_mbedtls --recurse-submodules https://github.com/Mbed-TLS/mbedtls.git mbedtls; then
+        break
+    else
+        sleep 5
+    fi
+done
 
 # libxml2
-[ ! -d libxml2 ] && git clone --depth 1 --branch v$v_libxml2 --recursive https://gitlab.gnome.org/GNOME/libxml2.git libxml2
+while true; do
+    rm -rf libxml2
+    if git clone --depth 1 --branch v$v_libxml2 --recursive https://gitlab.gnome.org/GNOME/libxml2.git libxml2; then
+        break
+    else
+        sleep 0.4
+    fi
+done
 
 # ffmpeg
-[ ! -d ffmpeg ] && git clone --depth 1 --branch n$v_ffmpeg https://github.com/FFmpeg/FFmpeg.git ffmpeg
+while true; do
+    rm -rf ffmpeg
+    if git clone --depth 1 --branch n$v_ffmpeg https://github.com/FFmpeg/FFmpeg.git ffmpeg; then
+        break
+    else
+        sleep 5
+    fi
+done
 
 cd ..
