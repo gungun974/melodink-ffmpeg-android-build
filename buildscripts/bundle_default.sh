58793618deed43ef1b002f7213becc5fbe6b35b0
Gungun974 <xfelix974@gmail.com>
2024-11-28 15:28:18 +0400

    Include headers in output

--- buildscripts/bundle_default.sh
+++ buildscripts/bundle_default.sh
@@ -23,9 +23,9 @@ cp flavors/default.sh scripts/ffmpeg.sh
 
 # --------------------------------------------------
 
-zip -r default-arm64-v8a.jar                prefix/arm64-v8a/usr/local/lib/*.so
-zip -r default-armeabi-v7a.jar              prefix/armeabi-v7a/usr/local/lib/*.so
-zip -r default-x86.jar                      prefix/x86/usr/local/lib/*.so
-zip -r default-x86_64.jar                   prefix/x86_64/usr/local/lib/*.so
+zip -r default-arm64-v8a.jar                prefix/arm64-v8a/usr/local/lib/*.so prefix/arm64-v8a/include
+zip -r default-armeabi-v7a.jar              prefix/armeabi-v7a/usr/local/lib/*.so prefix/armeabi-v7a/include
+zip -r default-x86.jar                      prefix/x86/usr/local/lib/*.so prefix/x86/include
+zip -r default-x86_64.jar                   prefix/x86_64/usr/local/lib/*.so prefix/x86_64/include
 
 md5sum *.jar
